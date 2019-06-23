7de09c83ffe6e0cb96cc0d0d23462460c60a5549
vector <747015457@qq.com>
2019-06-23 23:36:55 +0800

    Initial commit

---
+++ dubbo-api/src/main/java/com/ylkcs/service/DemoService.java
@@ -0,0 +1,13 @@
+package com.ylkcs.service;
+
+/**
+ * DemoService
+ * 服务Api接口类
+ * @author xiaoze
+ * @date 2018/6/6
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+}
---
+++ dubbo-provider/src/main/java/com/ylkcs/service/impl/DemoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ylkcs.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.ylkcs.service.DemoService;
+
+import java.util.logging.Logger;
+
+/**
+ * DemoServiceImpl
+ * 服务提供类
+ * @author xiaoze
+ * @date 2018/6/7
+ */
+@Service(version = "${demo.service.version}")
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name + " (from Spring Boot)";
+    }
+}
